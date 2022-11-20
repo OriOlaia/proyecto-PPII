43c8b58c59c40633cbc3b43a5c587f8714a38259
Oriana Olaia <olaoriana@gmail.com>
2022-11-19 23:35:27 -0300

    Modificando php y diseñando pagina

--- config.php
+++ config.php
@@ -1,20 +1,16 @@
 <?php
 
+    $con = mysqli_connect("localhost", "root", "", "gameover");
 
-$con = mysqli_connect("localhost", "root", "", "gameover");
+    define('USER', 'root');
+    define('PASSWORD', '');
+    define('HOST', 'localhost');
+    define('DATABASE', 'gameover');
 
-
-define('USER', 'root');
-define('PASSWORD', '');
-define('HOST', 'localhost');
-define('DATABASE', 'gameover');
-
-
-
-try {
-    $connection = new PDO("mysql:host=" . HOST . ";dbname=" . DATABASE, USER, PASSWORD);
-} catch (PDOException $e) {
-    exit("Error: " . $e->getMessage());
-}
+    try {
+        $connection = new PDO("mysql:host=" . HOST . ";dbname=" . DATABASE, USER, PASSWORD);
+    } catch (PDOException $e) {
+        exit("Error: " . $e->getMessage());
+    }
 
 ?>
--- register.php
+++ register.php
@@ -1,51 +1,52 @@
 <?php
 
-//Almaceno la conexión en una variable para usarla luego
-$conexion = mysqli_connect('localhost', 'root', '', 'gameover');
+    require 'config.php';
+    session_start();
+    if (isset($_POST['enviar'])) {
+        var_dump($_POST);
 
+        $nombre = $_POST['nombre'];
+        $apellido = $_POST['apellido'];
+        $email = $_POST['email'];
+        $password = $_POST['contrasena'];
+        $reppassword = $_POST['repcontra'];
 
-//Realizo la consulta almacenada en$query sobre la conexion almacenada en conexion
-$consulta = "SELECT * FROM register";
+        $query = $connection->prepare("SELECT * FROM register WHERE EMAIL=:email");
+        $query->bindParam("email", $email, PDO::PARAM_STR);
+        $query->execute();
 
-//Realizo la consulta almacenada en $query sobre la conexion almacenada en $conexion
-$resultado = mysqli_query($conexion, $consulta);
+        if ($query->rowCount() > 0) {
+            echo '<p class="error">The email address is already registered!</p>';
+        }
 
+        if ($query->rowCount() == 0) {
+            $query = $connection->prepare("INSERT INTO register (nombre,apellido,email,contrasena,repcontra) VALUES (:nombre,:apellido,:email,:contrasena, :repcontra)");
+            $query->bindParam(":nombre", $nombre, PDO::PARAM_STR);
+            $query->bindParam(":apellido", $apellido, PDO::PARAM_STR);
+            $query->bindParam(":email", $email, PDO::PARAM_STR);
+            $query->bindParam(":contrasena", $password, PDO::PARAM_STR);
+            $query->bindParam(":repcontra", $reppassword, PDO::PARAM_STR);
+            $result = $query->execute();
 
-require 'config.php';
-session_start();
-if (isset($_POST['enviar'])) {
-
-    $nombre = $_POST['nombre'];
-    $apellido = $_POST['apellido'];
-    $email = $_POST['email'];
-    $password = $_POST['contraseña'];
-    $reppassword = $_POST['repcontra'];
-
-    $query = $connection->prepare("SELECT * FROM register WHERE EMAIL=:email");
-    $query->bindParam("email", $email, PDO::PARAM_STR);
-    $query->execute();
-
-    if ($query->rowCount() > 0) {
-        echo '<p class="error">The email address is already registered!</p>';
-    }
-
-    if ($query->rowCount() == 0) {
-        $query = $connection->prepare("INSERT INTO register (nombre,apellido,email,contraseña,repcontra) VALUES (:nombre,:apellido,:email,:contraseña, :repcontra)");
-        $query->bindParam(":nombre", $nombre, PDO::PARAM_STR);
-        $query->bindParam(":apellido", $apellido, PDO::PARAM_STR);
-        $query->bindParam(":email", $email, PDO::PARAM_STR);
-        $query->bindParam(":contraseña", $password, PDO::PARAM_STR);
-        $query->bindParam(":repcontra", $reppassword, PDO::PARAM_STR);
-
-        $result = $query->execute();
-
-        if ($result) {
-            echo '<p class="success">Your registration was successful!</p>';
-        } else {
-            echo '<p class="error">Something went wrong!</p>';
+            if ($result) {
+                echo '<p class="success">Your registration was successful!</p>';
+            } else {
+                echo '<p class="error">Something went wrong!</p>';
+            }
         }
     }
-}
+    
+/*
+    require_once "config.php";
+
+    $nombre = $apellido = $email = $password = $reppassword = "";
+    $nombre_err  = $apellido_err = $email_err = $password_err = $reppassword_err = "";
+
+    if(empty(trim($_POST["email"]))){
+
+    }
+*/
+
 
 
 ?>
@@ -74,9 +75,10 @@ if (isset($_POST['enviar'])) {
             <input type="text" name="nombre" pattern="[a-zA-Z0-9]+" required>
             <input type="text" name="apellido" pattern="[a-zA-Z0-9]+" required>
             <input type="text" name="email" required>
-            <input type="password" name="contraseña" required>
+            <input type="password" name="contrasena" required>
             <input type="password" name="repcontra" required>
-            <button type="submit" acname="enviar" value="enviar">Registrar</button>
+            <button type="submit" name="enviar" value="enviar">Registrar</button>
+        
         </form>
     </div>
     <footer>
